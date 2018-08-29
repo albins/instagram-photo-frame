f0e35ac27f7aa5bc9f78d5afb9069aee3fcf1acf
Albin Stjerna <albin.stjerna@gmail.com>
2018-08-29 14:30:47 +0200

    Code review

--- static/slideshow.js
+++ static/slideshow.js
@@ -7,14 +7,9 @@ const FEED_REFRESH_INTERVAL_MS = 15000;
 function extractImage(post) {
     const id = post.id;
     const image_url = "/image/" + id;
-    const img = new Image(); // Bara själva referensen som är constant, inte properties
+    const img = new Image();
     img.src = image_url;
     img.id = "current-slide";
-    
-    // Det känns märkligt att pusha något till en parameter du får in.
-    // Du borde returnera för att undvika mutation så mycket som möjligt
-    //images.push(img);
-    
     return img;
 }
 
@@ -23,17 +18,12 @@ function extractFeed(posts) {
     posts.forEach((post) => {
         images.push(extractImage(post));
     });
-    
-    // Behöver du verkligen skicka med feed_processor hela vägen, kan du inte
-    // bara returnera och anropa cycleImages senare?
-    // feed_processor(images);
-    
     return images;
 }
 
 function getFeed() {
-    // Här borde du kunna använda `fetch` istället, men spelar väl ingen större roll
     fetch("/feed")
+        .then((response) => {return response.json()})
         .then((posts) => {
             const images = extractFeed(posts);
             cycleImages(0, images);
@@ -41,10 +31,6 @@ function getFeed() {
 }
 
 function setup() {
-    /* function start_cycling_images(images) {
-        cycleImages(0, images);
-    }*/
-
     getFeed();
     // set up a timer to continuously reload the feed:
     setInterval(getFeed, FEED_REFRESH_INTERVAL_MS);
@@ -53,6 +39,7 @@ function setup() {
 }
 
 function cycleImages(current_image, images) {
+    console.log(images);
     clearTimeout(imageTimeout);
     wrapped_index = current_image % images.length;
     const container_div = $('#content');
