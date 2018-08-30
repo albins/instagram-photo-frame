d3bb6a23ffa510e8b69da8639db884e55d191575
Albin Stjerna <albin.stjerna@gmail.com>
2018-08-30 09:25:12 +0200

    remove code reloading

--- static/slideshow.js
+++ static/slideshow.js
@@ -2,7 +2,6 @@ let imageTimeout;
 
 const IMAGE_SWITCH_INTERVAL_MS = 10000;
 const FEED_REFRESH_INTERVAL_MS = 600000; 
-const DOCUMENT_REFRESH_INTERVAL_MS = 4 * 60 * 60 * 1000;
 
 function extractImage(post) {
     const id = post.id;
@@ -23,11 +22,7 @@ function getFeed() {
 
 function setup() {
     getFeed();
-    // set up a timer to continuously reload the feed:
     setInterval(getFeed, FEED_REFRESH_INTERVAL_MS);
-
-    setInterval(() => {location.reload(true)},
-                DOCUMENT_REFRESH_INTERVAL_MS);
 
     document.getElementById("content")
         .onclick = () => {openFullscreen(document.body)};
